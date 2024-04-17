44da61a3935b011e5fed275400acb1b4820ac7f1
Dynesshely <catrol@qq.com>
2024-04-18 04:40:10 +0800

    Available to log to file

--- lib/logger/loggers/logger_file.dart
+++ lib/logger/loggers/logger_file.dart
@@ -1,12 +1,14 @@
-﻿import 'package:cherrilog/formatter/message.dart';
+﻿import 'dart:io';
+
+import 'package:cherrilog/formatter/message.dart';
 import 'package:cherrilog/logger/logger.dart';
 import 'package:cherrilog/model/message.dart';
 import 'package:dart_art/dart_art.dart';
 
 class CherriFile extends CherriLogger {
-  late String fileNamePattern = '@yyyy-@MM-@dd-@HH-@mm-@ss-@id.log';
+  late String fileNamePattern = '@yyyy-@MM-@dd-@HH-@id.log';
 
-  late String location = './logs/';
+  late String location = './logs';
 
   late Duration shelfLife = const Duration(days: 20);
 
@@ -18,15 +20,60 @@ class CherriFile extends CherriLogger {
 
   late int clearMode = CherriFileClearMode.oldFiles | CherriFileClearMode.outSizedFiles;
 
+  late int _fileId = 0;
+
+  CherriFile() {
+    _fileId = getNextId();
+  }
+
+  int getNextId() {
+    var lastIdFile = File('$location/options/last_id');
+    if (lastIdFile.existsSync()) {
+      var result = int.parse(lastIdFile.readAsStringSync());
+      lastIdFile.writeAsStringSync((result + 1).toString());
+      return result + 1;
+    } else {
+      lastIdFile
+        ..createSync(recursive: true)
+        ..writeAsStringSync('1');
+      return 1;
+    }
+  }
+
+  String getFileName() {
+    var now = DateTime.now();
+
+    var fileName = fileNamePattern
+        .replaceAll('@yyyy', now.year.toString().padLeft(4, '0'))
+        .replaceAll('@MM', now.month.toString().padLeft(2, '0'))
+        .replaceAll('@dd', now.day.toString().padLeft(2, '0'))
+        .replaceAll('@HH', now.hour.toString().padLeft(2, '0'))
+        .replaceAll('@mm', now.minute.toString().padLeft(2, '0'))
+        .replaceAll('@ss', now.second.toString().padLeft(2, '0'))
+        .replaceAll('@id', _fileId.toString());
+
+    var file = File('$location/$fileName');
+    if (file.existsSync()) {
+      var size = BinarySize()..bytesCount = file.lengthSync();
+      if (size > singleFileSizeLimit) {
+        _fileId = getNextId();
+        return getFileName();
+      }
+    }
+
+    return fileName;
+  }
+
   @override
   void log(CherriMessage message) {
     var formatter = CherriFormatterMessageDefault(
       timestampPattern: options.timeStampPattern,
       costumeSplitter: ' ',
+      autoColorize: false,
     );
 
     if (message.logLevel.inRange(options.logLevelRange)) {
-      var formattedMessage = formatter.format(message).replaceAll("\r", "");
+      var formattedMessage = formatter.format(message);
 
       for (var line in formattedMessage.split('\n')) {
         pushLine(line);
@@ -35,7 +82,15 @@ class CherriFile extends CherriLogger {
   }
 
   @override
-  void flush() {}
+  void flush() {
+    var file = File('$location/${getFileName()}');
+    if (file.existsSync() == false) {
+      file.createSync(recursive: true);
+    }
+
+    var content = '${buffer.join('\n')}\n';
+    file.writeAsStringSync(content, mode: FileMode.writeOnlyAppend);
+  }
 }
 
 class CherriFileClearMode {
